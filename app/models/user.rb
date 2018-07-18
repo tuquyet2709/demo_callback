56aed65a254ad2e16e204db6914068ebb6044110
tuquyet2709 <goldenlightning1252@gmail.com>
2018-07-18 12:46:40 +0700

    after initialize

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,34 @@ class User < ApplicationRecord
   end
   after_destroy { |user| logger.info( "=================User #{user.id} was destroyed=================" ) }
 
+  after_initialize do
+    puts ">>>>>>>>>>>>>> You have initialized an object!"
+  end
+
+  after_find do
+    puts ">>>>>>>>>>>>>> You have found an object!"
+  end
+
+  after_touch do
+    puts ">>>>>>>>>>>>>> You have touched an object"
+  end
+
+
+  after_save do
+    puts ">>>>>>>>>>>>>> after_save"
+  end
+
+  around_save :put_around
+
   private
 
   def upcase_name
     name.upcase!
   end
 
+  def put_around
+    puts ">>>>>>>>>>>>>> around_save -before"
+    yield
+    puts ">>>>>>>>>>>>>> around_save -after"
+  end
 end
